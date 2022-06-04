78ed32c44d2b4d0d92eebb203c98931518351bf5
Gioruiz11 <gioruiz11@hotmail.com>
2022-06-04 11:21:40 -0400

    Finished grid generation logic

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const grid = document.querySelector(".grid")
 
-// Loops to make 16x16 div grid square
-for(let i = 0; i < 256; i++) {
-const row = document.createElement("div");
-row.classList.add(`row`);
-grid.appendChild(row);
+// Generates 16x16 grid
+function divGenerator(){
+    let divSize = 16;
+    let divNumber = divSize * divSize;
+    let divWidth = (500 / divSize - 2);
+    for( let i = 0; i < divNumber; i++){
+        const div = document.createElement("div");
+        div.classList.add("square");
+        div.style.flex = `1 1 ${divWidth}px`;
+        div.style.border = `1px solid gray`;
+        grid.appendChild(div);
+    }
+
 }
+
+divGenerator();
 
 
 
