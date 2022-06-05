c3cd80124cba71a9e6c8f2aa829526cfb4723564
Gioruiz11 <gioruiz11@hotmail.com>
2022-06-05 15:48:16 -0400

    grid can now change sizes

--- script.js
+++ script.js
@@ -1,44 +1,33 @@
-const grid = document.querySelector(".grid")
+const grid = document.querySelector(".grid");
 
-// Calls grid creation function
-divGenerator();
+// create an initial 16x16 grid
+gridCreator(16);
 
-// Generates 16x16 grid
-function divGenerator(){
-    let divSize = 16;
-    let divNumber = divSize * divSize;
-    let divWidth = (500 / divSize - 2);
-    for( let i = 0; i < divNumber; i++){
-        const div = document.createElement("div");
-        div.classList.add("square");
-        div.style.flex = `1 1 ${divWidth}px`;
-        div.style.border = `1px solid gray`;
-        grid.appendChild(div);
+
+// Grid creator function
+function gridCreator(size){
+    if(size <= 100 && size >= 4){
+        let squareSize = 500 / size - 2;
+        for( let i = 0; i < (size*size); i++){
+            const newDiv = document.createElement("div");
+            newDiv.classList.add("square");
+            newDiv.style.border = "1px solid gray";
+            newDiv.style.flex = `1 1 ${squareSize}px`;
+            newDiv.addEventListener("mouseover", () => {
+            newDiv.style.backgroundColor = "black";
+        })
+        grid.append(newDiv);
     }
-
+ }
 }
 
-// On mouseover, square turns black
-const squares = document.querySelectorAll(".square");
-squares.forEach((square) => {
-    square.addEventListener("mouseover", function(e) {
-        console.log(e);
-        e.target.style.background = "black";
-    })
-})
-
-// Prompt after clicking grid resize button
-function PromptMe(){
-    let size = prompt("Enter a grid size between 4-100!", 16);
-    return size;
+// Change current grid size to new input grid size
+function changeSize(){
+    let newSize = prompt("Type in a new grid size, from 4-100!");
+    while(grid.firstChild){
+        grid.removeChild(grid.firstChild);
+    }
+    gridCreator(newSize);
 }
-
-
-
-
-
-
-
-
 
 
